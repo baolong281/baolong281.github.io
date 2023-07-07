7f52a10ef27c61dae74487cf9b2307cd438066be
dylan h <baolong281@gmail.com>
2023-07-06 23:27:24 -0500

    refactor content out of index page

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -19,3 +19,23 @@ export const getWorks = (): BlockSection[] => {
     },
   ];
 };
+
+export const getFun = (): BlockSection[] => {
+  const post1 = {
+    title: "img2math",
+    description: "chrome extension for converting images into TeX code",
+    links: [
+      {
+        sourceLink: "https://github.com/baolong281/img2math",
+        linkTitle: "github",
+      },
+    ],
+  };
+
+  return [
+    {
+      posts: [post1],
+      sectionTitle: "deep learning",
+    },
+  ];
+};
